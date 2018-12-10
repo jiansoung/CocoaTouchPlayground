300648d1d792026941f15ddd68ec79d97042af2b
jiansoung <jian.soung@outlook.com>
2018-12-10 18:06:27 +0800

    Update ViewController.swift

--- CocoaTouchPlayground/ViewController.swift
+++ CocoaTouchPlayground/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import Eureka
 
-class ViewController: UIViewController {
+class ViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        form +++ Section()
+            <<< TextRow() { row in
+                row.tag = "name"
+                row.title = "Name"
+                row.placeholder = "Enter your name"
+            }
+            <<< DateRow(){
+                $0.tag = "birthday"
+                $0.title = "Birthday"
+                $0.value = Date()
+            }
+        +++ Section()
+            <<< ButtonRow(){
+                $0.title = "Save"
+                $0.onCellSelection(self.buttonTapped(cell:row:))
+        }
     }
 
+    func buttonTapped(cell: ButtonCellOf<String>, row: ButtonRow) {
+        print("button tapped")
+        let nameRow: TextRow = form.rowBy(tag: "name") as! TextRow
+        let name = nameRow.value ?? ""
+        let dateRow: DateRow = form.rowBy(tag: "birthday") as! DateRow
+        let date = dateRow.value ?? Date()
+        print("name: \(name)")
+        print("date: \(date)")
+    }
 
 }
 
